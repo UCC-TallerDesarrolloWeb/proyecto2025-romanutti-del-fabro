b086e1e499fd4442b9f179dc44be665873bcc825
Andres <2416872@ucc.edu.ar>
2025-10-03 20:26:20 -0300

    Fix: Improve form validation and enhance navigation links

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -14,7 +14,7 @@ const errorVer = (event) => {
     const subjectInput = document.getElementById('subject');
     const messageInput = document.getElementById('message');
     
-    // Verificar si algún campo está vacío
+
     if (nameInput.value.trim() === '' || 
         emailInput.value.trim() === '' || 
         subjectInput.value.trim() === '' || 
@@ -22,18 +22,16 @@ const errorVer = (event) => {
         errorMessage.textContent = 'Por favor, completa todos los campos.';
         return;
     }
-    
-    // Verificar si el nombre contiene números
+
     if (isNaN(nameInput.value.trim()) === false) {
         errorMessage.textContent = 'Por favor, no introduzcas números en el nombre';
         return;
     }
-    
-    // Si llegamos aquí, el formulario es válido
+
     errorMessage.textContent = '';
     alert('Formulario enviado correctamente. ¡Gracias por contactarnos!');
     
-    // Opcional: limpiar el formulario
+
     nameInput.value = '';
     emailInput.value = '';
     subjectInput.value = '';
